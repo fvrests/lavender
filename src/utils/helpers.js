fe682d80679218f273422f830282d89d2a4b2b68
fvrests <jana.rusenovich@gmail.com>
2020-07-30 01:09:23 -0500

    add environment variables & hide keys

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,10 @@
 import store from '../store'
 
 // correcting interval - corrects compounding variation in time between ticks that would occur using setInterval
-export const setCorrectingInterval = (func, delay, init = false) => {
+export const setCorrectingInterval = (func, delay) => {
     var instance = {}
 
-    if (init) func()
+    // if (init) func()
 
     function tick(func, delay) {
         if (!instance.started) {
@@ -28,52 +28,17 @@ export const setCorrectingInterval = (func, delay, init = false) => {
     return tick(func, delay)
 }
 
-// fetch new position from geolocation - must be called on user input
-export async function fetchNewPosition() {
-    console.log('attempting fetch')
-    await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-            (pos) => {
-                console.log('pos', pos)
-                let { latitude, longitude } = pos.coords
-                store.commit('setPosition', { latitude, longitude })
-                resolve()
-            },
-            (err) => {
-                console.log('err', err)
-                reject(err)
-            },
-            { timeout: 5000, enableHighAccuracy: true }
-        )
-    })
-}
-
 // fetches current weather info from OpenWeatherMap API - max 60 calls/minute or 1,000,000 calls/month
 export function fetchWeather() {
     let baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-    let appid = 'de619c7b223045c3ad4bc7d8332d55ab'
-    console.log('stored values', store.state.position)
+    let appid = process.env.VUE_APP_WEATHER_KEY
+    // console.log('stored values', store.state.position)
     let { latitude, longitude } = store.state.position
     fetch(`${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${appid}`)
         .then((res) => res.json())
         .then((json) => {
             let weather = json
             store.commit('setWeather', weather)
-            console.log('weather set!', weather)
+            // console.log('weather set!', weather)
         })
-}
-
-/**
- *
- * @param {timestamp} timestamp
- * @param {number} maxAge
- */
-export function invalidateProperty(
-    timestamp,
-    maxAge = 30 * 60,
-    date = Date.now()
-) {
-    if (store.state.init) {
-        return !timestamp || date - timestamp >= maxAge
-    } else return
 }
