d81b0d9b54e423b72a5afce92de466634b495d8c
fvrests <fvrests@icloud.com>
2023-04-26 14:06:44 -0500

    migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '~': path.resolve(__dirname, './src'),
+        },
+    },
+})
