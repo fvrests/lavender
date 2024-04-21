92808ec599f21167c1f58befa9c7123b7501692a
fvrests <fvrests@icloud.com>
2024-04-21 15:01:16 -0500

    update import alias
    - Fixes "the CJS buld of Vite's Node API is deprecated"
    - See https://vueschool.io/articles/vuejs-tutorials/the-cjs-build-of-vites-node-api-is-deprecated/
    - See https://vueschool.io/articles/vuejs-tutorials/import-aliases-in-vite/

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'~': path.resolve(__dirname, './src'),
+			'~': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 })
